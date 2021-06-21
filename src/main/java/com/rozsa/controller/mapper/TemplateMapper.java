a4a9d9d37791d70feced05edc6f69098e1706b21
vitor.rozsa <vitor.rozsa@hotmail.com>
2021-06-21 08:30:55 -0300

    Save template data as object in database

--- src/main/java/com/rozsa/controller/mapper/TemplateMapper.java
+++ src/main/java/com/rozsa/controller/mapper/TemplateMapper.java
@@ -12,11 +12,14 @@ public interface TemplateMapper {
 
     @Mapping(target = "id", ignore = true)
     @Mapping(source = "id", target = "rawId")
+    @Mapping(target = "data", expression = "java(new org.bson.json.JsonObject(dto.getData()))")
     Template fromDto(TemplateDto dto);
 
     @Mapping(source = "rawId", target = "id")
+    @Mapping(target = "data", expression = "java(template.getData().getJson())")
     TemplateDto toDto(Template template);
 
     @Mapping(source = "rawId", target = "id")
+    @Mapping(target = "data", expression = "java(template.getData().getJson())")
     List<TemplateDto> toDtos(List<Template> templates);
 }
--- src/main/java/com/rozsa/model/Template.java
+++ src/main/java/com/rozsa/model/Template.java
@@ -1,8 +1,11 @@
 package com.rozsa.model;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
+import org.bson.json.JsonObject;
 
+@EqualsAndHashCode(callSuper = true)
 @Data
 public class Template extends BaseModel {
-    private String data;
+    private JsonObject data;
 }
