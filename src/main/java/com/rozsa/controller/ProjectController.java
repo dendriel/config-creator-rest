a452b8cc664ef8fec1423678498920ae20057bc3
vitor.rozsa <vitor.rozsa@hotmail.com>
2021-06-28 20:23:36 -0300

    Add get API for default user project

--- src/main/java/com/rozsa/controller/ProjectController.java
+++ src/main/java/com/rozsa/controller/ProjectController.java
@@ -4,6 +4,10 @@ import com.rozsa.business.ProjectBusiness;
 import com.rozsa.controller.dto.BaseDto;
 import com.rozsa.controller.mapper.ProjectMapper;
 import com.rozsa.model.Project;
+import com.rozsa.security.UserContext;
+import org.bson.types.ObjectId;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,5 +17,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class ProjectController extends BaseController<Project, BaseDto, ProjectBusiness, ProjectMapper> {
     public ProjectController(ProjectBusiness business, ProjectMapper mapper) {
         super(business, mapper);
+    }
+
+    @GetMapping
+    public ResponseEntity<BaseDto> get() {
+        ObjectId projectId = UserContext.getDefaultProjectId();
+        Project data = business.find(projectId);
+        if (data == null) {
+            return ResponseEntity.notFound().build();
+        }
+
+        BaseDto dto = mapper.toDto(data);
+        return ResponseEntity.ok().body(dto);
     }
 }
